2b05dd8056ec61d56edc86c50d5a69e09f76042e
Jorge <jorgeperezlj@gmail.com>
2021-04-23 21:20:06 -0600

    routing system for auth done

--- games-site/src/app/app-routing.module.ts
+++ games-site/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)},
+  {path: '**', redirectTo: 'auth'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
